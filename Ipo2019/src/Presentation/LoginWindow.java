b0e2fd09b422ab4cfc7bb1f2e2b7a5ee01bf25ed
Samuglz6 <samu_sgl_96@hotmail.com>
2019-04-13 19:16:17 +0200

    minor changes

--- Ipo2019/src/Presentation/LoginWindow.java
+++ Ipo2019/src/Presentation/LoginWindow.java
@@ -31,11 +31,14 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dialog.ModalExclusionType;
+import javax.swing.border.EmptyBorder;
 
 public class LoginWindow
 {
 
 	JFrame frameLogin;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	/**
 	 * Launch the application.
@@ -94,15 +97,107 @@ public class LoginWindow
 		gridBagLayout.columnWidths = new int[]{0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		frameLogin.getContentPane().setLayout(gridBagLayout);
 		
+		JPanelBackground panel = new JPanelBackground();
+		panel.setBackground(LoginWindow.class.getResource("/Resources/Images/login_background.png").toString());
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		
+		panel.setBackground(Color.WHITE);
+		panel.setForeground(Color.WHITE);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		frameLogin.getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{50, 0, 50, 0};
+		gbl_panel.rowHeights = new int[]{40, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Restaurante El Gato Negro");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 1;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(LoginWindow.class.getResource("/Resources/Images/login_background.png")));
+		label.setIcon(new ImageIcon(LoginWindow.class.getResource("/Resources/Images/logo_icon.png")));
 		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.gridx = 0;
-		gbc_label.gridy = 0;
-		frameLogin.getContentPane().add(label, gbc_label);
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 1;
+		gbc_label.gridy = 3;
+		panel.add(label, gbc_label);
+		
+		JPanelBackground panel_1 = new JPanelBackground();
+		panel_1.setBackground(LoginWindow.class.getResource("/Resources/Images/login_background.png").toString());
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridy = 5;
+		panel.add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{15, 0, 15, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 5, 0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JLabel lblNewLabel = new JLabel("Inicia Sesi\u00F3n");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		panel_1.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 3;
+		panel_1.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 5;
+		panel_1.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(LoginWindow.class.getResource("/Resources/Images/login_icon.png")));
+		btnNewButton.setOpaque(false);
+		btnNewButton.setContentAreaFilled(false);
+		btnNewButton.setBorderPainted(false);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 6;
+		panel_1.add(btnNewButton, gbc_btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("Registrarse ahora");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 7;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Recuperar contrase\u00F1a");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 1;
+		gbc_lblNewLabel_3.gridy = 8;
+		panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
 		
 		//panel.setBackground(MainWindow.class.getResource("/Resources/Images/login_background.png").toString());
